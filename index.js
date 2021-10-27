8633611c762fcb9e4f632c5bd3d0e931bce3986d
Tala-coder <muhammedtalic.it@gmail.com>
2021-10-27 09:18:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ for (let index = 0; index < 10; index++) {
     let poruka = "ovo je broj " + index;
     console.log(poruka);
 }
+
+console.log("--------------------------------------------------------")
+
+const student = {
+    indeks : '117/IT-19',
+    ime : 'Muhammed',
+    prezime : 'Talic',
+    ocjene : [
+        { predmet : ' Programiranje I', ocjena: 9},
+        { predmet : 'Baze podataka', ocjena: 8 }
+    ]
+}
+
+const kljuceviObjekta = Object.getOwnPropertyNames(student);
+console.log(kljuceviObjekta);
+
+for(let kljuc of kljuceviObjekta){
+    let podatak = student[kljuc];
+    console.log('naziv kljuca je ', kljuc , ' i pod tim kljucem je ', podatak);
+}
+
+
+const JSON_zapisi_studenata = JSON.stringify(student);
+console.log(JSON_zapisi_studenata)
+console.log(JSON.stringify(student))
+
+console.log("------------------------  FUNCKIJE  --------------------------------")
+
