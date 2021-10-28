fc24397ce8dd92a1a3b2d4ed8f39fdd409838aa8
Tala-coder <muhammedtalic.it@gmail.com>
2021-10-29 00:17:40 +0200

    local storage

--- Slike_kursa/ponavljanje-js/main.js
+++
@@ -1,9 +0,0 @@
-function init(){
-    prikaziCuvanjePodatakaPopup();
-}
-
-window.addEventListener('load', init);
-function prikaziCuvanjePodatakaPopup(){
-    let d = document.getElementById('cuvanjePodataka');
-    d.classList.remove('hidden');
-}
