d5f55c536a846918814049b095467db560af2b24
Tala-coder <muhammedtalic.it@gmail.com>
2021-10-31 23:45:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'aplikacija',
+    password: 'aplikacija',
+    database:'aplikacija'
+});
+
+connection.promise().query('SELECT * FROM category WHERE parent__category_id = ?;', [1])
+.then(prikaziKategorije)
+.catch(error => {
+    console.log('Doslo je do greske ', error);
+})
+
+function prikaziKategorije([rows, fields] ){ // rows redovi koje citamo, mogli smo staviti i categories npr.|| fields -kolone
+    for (let category of rows) {
+        console.log('Ucitana kategorija je: ', category.name);
+    }
+}
