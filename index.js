deeabb3b2fff985a0393d5258e49bd7fb93cc5ca
Tala-coder <muhammedtalic.it@gmail.com>
2021-11-01 13:45:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,7 @@ const http = require('http');
 const server = http.createServer(requestHandler);
 
 function requestHandler(request, response){
-    const podaciOdKlijenta = {
-        method: request.method,
-        link: request.url
-    };
 
-    const podaciOdKlijentaJson = JSON.stringify(podaciOdKlijenta);
-
-    response.writeHead(200, {
-        'Content-type': 'application/json; charset=utf-8'
-    });
-
-    response.write(podaciOdKlijentaJson);
-    response.end();
 }
 
 server.listen(443);
