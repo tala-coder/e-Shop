ce8e07f392e9c565354498042b078768d05d69db
Tala-coder <muhammedtalic.it@gmail.com>
2021-12-19 22:23:19 +0100

    Create proizvod.js

---
+++ eShop_Muhammed_Talic/models/proizvod.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+
+const recenzijaSchema = mongoose.Schema(
+    {
+        korisnik: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Korisnik",
+        },
+        ime: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+        komentar: {
+            type: String,
+            required: true,
+        },
+        /*vrijemeKreiranja: {
+            type: Date,
+            default: Date.now,
+        },*/
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const proizvodSchema = mongoose.Schema({
+    naziv: {
+        type: String,
+        required: true,
+    },
+    opis: {
+        type: String,
+        required: true
+    },
+    detaljnjiOpis: {
+        type: String,
+        default: ''
+    },
+    brand: {
+        type: String,
+        default: ''
+    },
+    cijena : {
+        type: Number,
+        default:0
+    },
+    kategorija: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Kategorija',
+        required:true
+    },
+    kolicina: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    },
+/*    rating: {
+        type: Number,
+        default: 0,
+    },*/
+    brojRecenczija: {
+        type: Number,
+        default: 0,
+    },
+    slika: {
+        type: String,
+        default: ''
+    },
+    slike: [{
+        type: String
+    }],
+    istaknut: {
+        type: Boolean,
+        default: false,
+    },
+    datumKreiranja: {
+        type: Date,
+        default: Date.now,
+    },
+    reviews: [recenzijaSchema],
+})
+
+
+Proizvod = mongoose.model('Proizvod', proizvodSchema);
+module.exports = Proizvod;
