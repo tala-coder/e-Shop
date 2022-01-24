c40823c310dba16455f73bc4fa6db964e39c0f44
Tala-coder <muhammedtalic.it@gmail.com>
2022-01-24 19:19:59 +0100

    moment socket

--- eShop_Muhammed_Talic/routes/soketi.js
+++ eShop_Muhammed_Talic/routes/soketi.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var io = null;
+const {generateMessage} = require('../helpers/soketi')
 
 
 router.get('/',
@@ -8,15 +9,21 @@ router.get('/',
     if (!io){
         io = require('socket.io')(req.socket.server);
         io.sockets.on('connection', (socket) => {
-            socket.emit('poruka', ' Dobro dosli na chat talashop');
-            socket.broadcast.emit('poruka', 'Chatu se pridruzila nova osoba');
+            socket.emit('poruka', generateMessage('Dobro došli na chat TalaShop'));
+
+            socket.broadcast.emit('poruka', generateMessage('Chatu se pridruzila nova osoba'));
 
             socket.on('posaljiPoruku', (msg) => {
-                io.emit('poruka', msg)
+                io.emit('poruka', generateMessage(msg))
+            })
+
+            socket.on('sendLocation', (coords) => {
+                io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+                // callback()
             })
 
             socket.on('disconnect', () => {
-                io.emit('poruka', 'korisnik je napustio chat')
+                io.emit('poruka', generateMessage('korisnik je napustio chat'))
             })
         })
     }
