1c0c8f4a72a2854b1c4b1dbdea96fccdee9ad929
Tala-coder <muhammedtalic.it@gmail.com>
2022-01-26 02:05:56 +0100

    Update mongoConnect.js

--- helpers/mongoConnect.js
+++ helpers/mongoConnect.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 /*https://stackoverflow.com/questions/55267494/the-uri-parameter-to-openuri-must-be-a-string-got-undefined/55267839*/
 const dotenv = require('dotenv')
 dotenv.config({path:__dirname+'/.env'});
-const mongoConnectionUrl = process.env.MONGO_CONNECTION_URL;
+// const mongoConnectionUrl = process.env.MONGO_CONNECTION_URL;
 
 exports.connectDB = async () => {
     try {
-        const connect = await mongoose.connect(mongoConnectionUrl);
+        const connect = await mongoose.connect('mongodb+srv://muhammed:tala123@muhammedtaliceshop.soks2.mongodb.net/eshop-database?retryWrites=true&w=majority');
         console.log('\x1b[34m', ` Spajanje sa bazom '${connect.connection.name}' je uspjelo!
   ${process.env.linkShopa}${process.env.nazivShopa}`
         );
